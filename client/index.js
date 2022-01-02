8b8e3bdabf53fc95cff8152dadfe60b54dc916fe
Thankgod20 <vwegbathankgod@gmail.com>
2022-01-02 11:19:23 -0500

    Updates for Flashloan Smartcontract test

--- client/index.js
+++ client/index.js
@@ -2,38 +2,56 @@ const Web3 = require('web3');
 const Mycontract = require('../build/contracts/Flashswap.json');
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const mnemonic = require("../secrets.json").mnemonic;
+const prompt = require('prompt');
+var tokenAddr;
+var to;
+var amount;
+prompt.start();
 
-const readline = require('readline').createInterface(
-    {
-        input: process.stdin,
-        output: process.stdout
-    }
-)
 var provider = new HDWalletProvider(mnemonic, `https://speedy-nodes-nyc.moralis.io/346380c8eca1a345a08fbdc8/eth/ropsten`);
+
 const init = async () => {
     const web3 = new Web3 (provider); //('https://speedy-nodes-nyc.moralis.io/346380c8eca1a345a08fbdc8/eth/ropsten');
     const id = await web3.eth.net.getId();
     //console.log("NetworkId_"+id);
-    const ContractAddr = '0x7c385f1b43656C04a0e3F93f9bF2C9e15D178b53';
+    const ContractAddr = '0x19E6371a316fC610735d4AB81eD6DE4A2eAa33c9';
     const contract = new web3.eth.Contract(
         Mycontract.abi,
         ContractAddr
     );
 
     const address = await web3.eth.getAccounts();
-    console.log("Current Address:-",address);
+    console.log("Current Address:-",address[0]);
+    const addressBalance = await web3.eth.getBalance(address[0]);
+    console.log("Current Account Balance:-",addressBalance);
     
-    readline.question('Enter Token Borrow Address:-',(tokenAddr)=> {
-            contract.methods
-                        .getTokenPair(tokenAddr)
-                        .call()
-                        .then(result => {
-                            console.log ("Token Pair Exisit:-",result);
-                        } );
+    prompt.get(['tokenAddress','to','amount'], (err,result) => {
+        if (err) {
+            return onErr(err);
+        }
+        tokenAddr = result.tokenAddress;
+        to = result.to;
+        amount = result.amount;
 
-        readline.close(); 
+        try {
+            contract.methods
+                .initFlashloan(tokenAddr, to, amount)
+                .send({from:address[0],gas:300000,gasPrice:null});
+        } catch (err_) {
+            return onErr(err_);
+        }
+        
+             
     });
+
+
  
   
 }
+
+const onErr = (err) => {
+    console.log(err);
+    return 
+}
+
 init();
